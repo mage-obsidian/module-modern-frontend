70986f6709fc1bd2230711b06f7b16a02895f678
Jeanmarcos Juarez <janmarcoj@gmail.com>
2025-04-07 00:50:26 -0400

    feat: Update configuration files and add module CSS for improved styling

--- src/Api/Data/ConfigInterface.php
+++ src/Api/Data/ConfigInterface.php
@@ -13,20 +13,20 @@ interface ConfigInterface
     public const string VUE_COMPONENTS_PATH = 'components';
     public const string JS_PATH = 'js';
     public const array FOLDERS_TO_WATCH = [
-        self::VUE_COMPONENTS_PATH,
-        self::JS_PATH
+            self::VUE_COMPONENTS_PATH,
+            self::JS_PATH
     ];
     public const array ALLOWED_EXTENSIONS = [
-        'js',
-        'vue',
-        'cjs',
-        'ts'
+            'js',
+            'vue',
+            'cjs',
+            'ts'
     ];
     public const string GENERATED_PATH = 'generated';
     public const string VITE_GENERATED_PATH = 'vite_generated';
     public const string MODULE_CSS_EXTEND_FILE = 'module.extend.css';
-    public const string MODULE_CONFIG_FILE = 'module.config.cjs';
-    public const string THEME_CONFIG_FILE = 'theme.config.cjs';
+    public const string MODULE_CONFIG_FILE = 'module.config.js';
+    public const string THEME_CONFIG_FILE = 'theme.config.js';
     public const string THEME_CSS_SOURCE_FILE = 'theme.source.css';
     public const string THEME_FILES_PATH = 'Theme';
     public const string LIB_PATH = 'lib';
